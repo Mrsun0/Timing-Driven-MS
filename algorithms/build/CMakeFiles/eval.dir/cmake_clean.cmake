ea653183507d103101abf4856380cbc0465e3316
gwSun <18863013890@163.com>
2023-09-17 21:26:33 +0800

    version 0.1

---
+++ algorithms/build/CMakeFiles/eval.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/eval.dir/eval.c.o"
+  "libeval.pdb"
+  "libeval.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/eval.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
